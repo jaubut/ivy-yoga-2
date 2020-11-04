4c46dfc4be8411a85280a851fafed57e98e2d89a
jaubut <jeremie.aubut@gmail.com>
2020-11-03 21:09:32 -0500

    ajustement

--- components/classe-du-jour.vue
+++ components/classe-du-jour.vue
@@ -7,6 +7,7 @@
       </p>
       <h3>Nos classes aujourd'hui</h3>
       <p>Si tu n'as pas d'abonnement, tu vas pouvoir en choisir un lors de ta réservation.</p>
+      <p><img height="auto" width="25px" src="https://images.prismic.io/ivy-yoga/df016b1b-8224-43b4-ad66-8ee263f61487_camera.png?auto=compress,format" alt="">  Cours en studio et aussi diffusé en <a href="https://www.facebook.com/groups/2350913745213806/" target="_blank">ligne</a>.</p>
       <a class="ps-button ps-button--primary" href="https://mindbody.io/locations/ivy-espace-yoga-arts-communaute" target="_blank">Réserver</a>
     </div>
   </div>

9cfa01f629cbd604c1ca3c0594bb995367dffc1d
jaubut <jeremie.aubut@gmail.com>
2020-11-21 23:40:05 -0500

    some chamges

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
       url="https://images.prismic.io/ivy-yoga/0e5d227e-6d0b-4eda-b1f8-feea573ac806_hero-1.jpg?auto=compress,format"
       title="Durant la période en Zone Rouge, nous continuons de yogater en ligne !"
       description=""
-      ctaurl="https://mindbody.io/locations/ivy-espace-yoga-arts-communaute"
-      cta="Réserver ma classe"
+      ctaurl="/tarifs"
+      cta="En savoir plus"
     />
     <slice-zone type="page" uid="accueil" />
     <classe-jour />
--- pages/tarifs.vue
+++ pages/tarifs.vue
@@ -4,8 +4,8 @@
       url="https://images.prismic.io/ivy-yoga/ebb2f599-67bf-4937-8d8d-d1b8d5093e1a_cours+en+ligne.jpg?auto=compress,format"
       title="Yoga virtuel"
       description="Durant la période en Zone Rouge, nous continuons de yogater en ligne. Viens nous rejoindre dès maintenant !"
-      ctaurl="https://mindbody.io/locations/ivy-espace-yoga-arts-communaute"
-      cta="Réserver une classe"
+      ctaurl="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100068"
+      cta="Accède à plus de 100 classes"
     />
     <slice-zone type="page" uid="tarifs1" />
   </div>

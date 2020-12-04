76da92b14bea20995ba7ac4833ae5ac98bc6022d
jaubut <jeremie.aubut@gmail.com>
2020-12-04 14:31:18 -0500

    try to fix again the carousel

--- plugins/vue-carousel.js
+++ plugins/vue-carousel.js
@@ -1,4 +1,3 @@
 import Vue from 'vue'
-import { Carousel, Slide } from 'vue-carousel'
-Vue.component('carousel', Carousel)
-Vue.component('slide', Slide)
+import VueCarousel from 'vue-carousel'
+Vue.use(VueCarousel)

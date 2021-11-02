7645c2d9a4ce37650e219196799a3b94544b7708
jaubut <jeremie.aubut@gmail.com>
2021-11-02 16:19:43 -0400

    new version of the phrasing

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <hero-promo
       url="https://images.prismic.io/ivy-yoga/0e5d227e-6d0b-4eda-b1f8-feea573ac806_hero-1.jpg?auto=compress,format"
       title="Abonnement Illimité à partir de 72 $ / mois"
-      title2="À partir de 10 $ / classe"
-      description="sans abonnement"
+      title2="classe à partir de 10 $ sans abonnement"
+      description=""
       ctaurl="/tarifs"
       cta="Voir toutes les offres"
     />
